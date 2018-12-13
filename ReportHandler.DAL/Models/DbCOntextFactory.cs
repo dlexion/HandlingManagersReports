0025a2bba2f13ec24bde554b06a3e1099d46c4c1
dlexion <dlexion@yandex.ru>
2018-12-13 08:32:16 +0300

    Add DbContextFactory

---
+++ ReportHandler.DAL/Models/DbCOntextFactory.cs
@@ -0,0 +1,19 @@
+﻿using ReportHandler.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportHandler.DAL.Models
+{
+    public class DbContextFactory : IDbContextFactory
+    {
+        public DbContext CreateInstance()
+        {
+            return new ReportModelContainer();
+        }
+    }
+}
