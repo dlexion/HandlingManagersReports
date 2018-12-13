236f7851103a04f2cff3918bd1857bc26c7973d4
dlexion <dlexion@yandex.ru>
2018-12-13 11:41:54 +0300

    Add GenericUnitOfWork and AutoMapperProfile

---
+++ ReportHandler.BLL/Models/AutoMapperProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ReportHandler.BLL.DTO;
+using ReportHandler.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportHandler.BLL.Models
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Customer, CustomerDTO>();
+            CreateMap<CustomerDTO, Customer>()
+                .ForMember(x => x.Orders, option => option.Ignore());
+
+            CreateMap<Item, ItemDTO>();
+            CreateMap<ItemDTO, Item>()
+                .ForMember(x => x.Orders, option => option.Ignore());
+
+            CreateMap<Manager, ManagerDTO>();
+            CreateMap<ManagerDTO, Manager>()
+                .ForMember(x => x.Orders, option => option.Ignore());
+
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderDTO, Order>()
+                .ForMember(x => x.ManagerId, option => option.Ignore())
+                .ForMember(x => x.CustomerId, option => option.Ignore());
+        }
+
+    }
+}
