352ced7311ce7355cdbfa8dc63d3e219bcbe1edd
dlexion <dlexion@yandex.ru>
2018-12-16 21:50:50 +0300

    Add SimpleInjector DI Container to Console app

--- ReportHandler.PL.Console/Program.cs
+++ ReportHandler.PL.Console/Program.cs
@@ -1,25 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ReportHandler.BLL;
-using ReportHandler.BLL.Models;
+﻿using ReportHandler.BLL.Models;
 using ReportHandler.DAL.AutoMapperSetup;
 using ReportHandler.DAL.Contracts.DTO;
+using ReportHandler.DAL.Contracts.Interfaces;
 using ReportHandler.DAL.Models;
+using SimpleInjector;
 
 namespace ReportHandler.PL.Console
 {
     class Program
     {
+        static readonly Container container;
+
+        static Program()
+        {
+            container = new Container();
+
+            container.RegisterInstance<IUnitOfWorkFactory>(new UnitOfWorkFactory());
+            container.Register<FileHandler>();
+
+            container.Verify();
+        }
+
         static void Main(string[] args)
         {
             AutoMapperConfiguration.Configure();
 
             // TODO take parameters from config
-            var watcher = new FolderWatcher(@"D:\TestFolder", "*.csv", new FileHandler());
+            var watcher = new FolderWatcher(@"D:\TestFolder", "*.csv", container.GetInstance<FileHandler>());
             watcher.Start();
 
             System.Console.ReadKey();
