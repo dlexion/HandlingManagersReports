cb82801749ce1c624bc9f0e530fb916e5154ca6b
dlexion <dlexion@yandex.ru>
2018-12-16 21:12:22 +0300

    Create windows service assembly

---
+++ ReportHandler.PL.Service/ReportHandlerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportHandler.PL.Service
+{
+    public partial class ReportHandlerService : ServiceBase
+    {
+        public ReportHandlerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            File.AppendAllText(@"D:\TestFolder\1.txt","It works");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
