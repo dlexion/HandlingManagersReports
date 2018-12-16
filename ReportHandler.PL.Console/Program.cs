b39bde6a10757a1be76ef0cec84147cfaba0244c
dlexion <dlexion@yandex.ru>
2018-12-16 19:51:52 +0300

    Create Console application and start watching folder

---
+++ ReportHandler.PL.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReportHandler.BLL;
+using ReportHandler.BLL.Models;
+using ReportHandler.DAL.AutoMapperSetup;
+using ReportHandler.DAL.Contracts.DTO;
+using ReportHandler.DAL.Models;
+
+namespace ReportHandler.PL.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoMapperConfiguration.Configure();
+
+            // TODO take parameters from config
+            var watcher = new FolderWatcher(@"D:\TestFolder", "*.csv", new FileHandler());
+            watcher.Start();
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ ReportHandler.PL.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReportHandler.PL.Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportHandler.PL.Console")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5110b7aa-0a21-4aba-92d0-88d660128361")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
