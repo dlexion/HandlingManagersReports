b54c791a6397062d3b1801f495bf3a6294b3d4f3
dlexion <dlexion@yandex.ru>
2018-12-16 21:33:01 +0300

    Create factory that implements IUnitOfWorkFactory interfaace

---
+++ ReportHandler.DAL/Models/UnitOfWorkFactory.cs
@@ -0,0 +1,12 @@
+﻿using ReportHandler.DAL.Contracts.Interfaces;
+
+namespace ReportHandler.DAL.Models
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        public IUnitOfWork GetInstance()
+        {
+            return new UnitOfWork();
+        }
+    }
+}
